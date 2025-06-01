a0a56255fa39824698a97051468cb492344b528a
Mohamad Suleyman <mohamad.sleman1999@gmail.com>
2025-06-01 23:22:36 +0300

    adding credentials

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 $host = 'sql109.infinityfree.com';
-$db = 'student_portal';
+$db = 'f0_39120594_studentdb';
 $user = 'if0_39120594';
 $pass = 'w3XaA8wJIuPQN'; // Default MAMP password
-$port = 3306;   // Default MAMP MySQL port
+$port = 3306;  // Default MAMP MySQL port
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;port=$port", $user, $pass);
